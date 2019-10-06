39ad58b5d4e75169c0a1a06010143eff73f5858d
abesshapov <45789410+abesshapov@users.noreply.github.com>
2019-10-06 11:12:07 +0300

    Add files via upload
    Файл с работой по stepik

---
+++ stepikMod2.py
@@ -0,0 +1,34 @@
+import time
+from math import sin, log
+from local_settings import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    browser = webdriver.Firefox(
+                executable_path=ep, 
+                firefox_binary=fb
+    )
+    browser.get(link)
+    button = WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), '100')
+    )     
+    browser.find_element_by_id("book").click()
+    button = browser.find_element_by_id("solve")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    x = int(browser.find_element_by_id("input_value").text)
+    res = str(log(abs(12*sin(x))))
+    browser.find_element_by_id("answer").send_keys(res)
+    xp = '//button[contains(text(), "Submit")]'
+    button = browser.find_element_by_xpath(xp)
+    button.click()        
+    a = browser.switch_to.alert
+    print(a.text)
+finally:
+    time.sleep(5)
+    browser.quit()
